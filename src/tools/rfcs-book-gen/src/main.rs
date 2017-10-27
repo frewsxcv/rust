7e68aa363f07eb4ee8d76da8d3359c2a886d64e3
Corey Farwell <coreyf@rwell.org>
2017-10-27 12:08:08 -0400

    f

--- src/tools/rfcs-book-gen/src/main.rs
+++ src/tools/rfcs-book-gen/src/main.rs
@@ -1,4 +1,5 @@
 use std::{env, error, fs, path};
+use std::io::Write;
 
 fn run<S, D>(src_path: S, dest_path: D) -> Result<(), Box<error::Error>>
 where
@@ -10,25 +11,30 @@ where
 
     fs::create_dir_all(&dest_path)?;
 
-    let mut rfcs_file_names =
-        src_path
-            .as_ref()
-            .read_dir()?
-            .map(|d| d.unwrap().path().file_name().unwrap().to_owned())
-            .collect::<Vec<_>>();
+    let mut rfcs_file_names = src_path
+        .as_ref()
+        .read_dir()?
+        .map(|d| d.unwrap().path().file_name().unwrap().to_owned())
+        .collect::<Vec<_>>();
 
     rfcs_file_names.sort();
 
     for i in rfcs_file_names {
-        buf.push_str(
-            &format!(
-                "- {}\n",
-                i.to_str().unwrap()
-            )
-        );
+        buf.push_str(&format!(
+            "- [{}]({})\n",
+            i.to_str().unwrap().trim_right_matches(".md"),
+            i.to_str().unwrap()
+        ));
     }
 
-    println!("{}", buf);
+    let mut file = fs::File::create(dest_path.as_ref().join("SUMMARY.md"))?;
+    file.write_all(buf.as_bytes())?;
+
+    for d in src_path.as_ref().read_dir()? {
+        let d = d.unwrap();
+        fs::copy(d.path(), dest_path.as_ref().join(d.file_name())).expect("could not copy");
+    }
+
     Ok(())
 }
 
@@ -36,8 +42,10 @@ fn main() {
     let src_path_str = env::args_os().skip(1).next().expect("source path required");
     let src_path = path::Path::new(&src_path_str);
 
-    let dest_path_str = env::args_os().skip(2).next().expect("destination path required");
+    let dest_path_str = env::args_os().skip(2).next().expect(
+        "destination path required",
+    );
     let dest_path = path::Path::new(&dest_path_str).join("src");
 
-    run(src_path, dest_path);
+    run(src_path, dest_path).unwrap();
 }
